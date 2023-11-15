adf5a9a9205b2b9ee9152ebbc4778ba594d75283
Jiwon <wldnjs980409@naver.com>
2023-11-15 11:47:14 +0900

    [feat] Output View

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,8 @@
+package christmas.view;
+
+public class OutputView {
+
+    public static void printEventMessage(String visitDay){
+        System.out.println("12월 " + visitDay + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+}

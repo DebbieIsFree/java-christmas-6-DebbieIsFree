c697fc8583f2a45aaefa0e26300713d5a1192770
Jiwon <wldnjs980409@naver.com>
2023-11-15 11:48:43 +0900

    [feat] Application

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.controller.PlayController;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayController playController = new PlayController();
+
+        playController.start();
     }
 }
